c0b0150baf417bb5e9acc3dcf8fede4454ba51b2
JGA <jonathan.gass@sanalogic.com>
2024-03-13 12:07:04 +0100

    Erster Ansatz der Anwesenheiten

--- ColtUI/src/app/attendances/attendances.component.ts
+++ ColtUI/src/app/attendances/attendances.component.ts
@@ -1,11 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { LayoutComponent } from '../core/layout/layout.component';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { Attendance, Department, User } from '../shared';
 
 @Component({
   selector: 'app-attendances',
   standalone: true,
-  imports: [],
+  imports: [TableModule, DropdownModule],
   templateUrl: './attendances.component.html',
   styleUrl: './attendances.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AttendancesComponent {}
+export class AttendancesComponent implements OnInit {
+  constructor(private layoutComponent: LayoutComponent) {}
+
+  attandences!: Attendance[];
+  departments!: Department[];
+  users!: User[];
+
+  ngOnInit(): void {
+    this.layoutComponent.cardHeader = 'Anwesenheiten';
+
+    this.attandences = [
+      {
+        user_id: 1,
+        date: '13.03.2024',
+        month: 'März',
+        department_id: 1,
+        id: 1,
+      },
+    ];
+  }
+}
