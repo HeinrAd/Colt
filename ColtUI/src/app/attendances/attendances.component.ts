e236371c220470fa5a39b42f1e0d3b7c1da8d8f2
JGA <jonathan.gass@sanalogic.com>
2024-03-18 08:20:58 +0100

    Layout der Anwesenheits-Seite angepasst

--- ColtUI/src/app/attendances/attendances.component.ts
+++ ColtUI/src/app/attendances/attendances.component.ts
@@ -11,11 +11,14 @@ import { Attendance, Department, User } from '../shared';
 import { CalendarModule } from 'primeng/calendar';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-attendances',
   standalone: true,
   imports: [
+    CommonModule,
     TableModule,
     DropdownModule,
     CalendarModule,
@@ -27,15 +30,22 @@ import { ButtonModule } from 'primeng/button';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AttendancesComponent implements OnInit {
-  constructor(private layoutComponent: LayoutComponent) {}
+  constructor(
+    private primengConfig: PrimeNGConfig,
+    private layoutComponent: LayoutComponent
+  ) {}
 
   attandences!: Attendance[];
   departments!: Department[];
   users!: User[];
   selectedDate = signal<Date | null>(null);
+  currentDate!: Date;
 
   ngOnInit(): void {
     this.layoutComponent.cardHeader = 'Anwesenheiten';
+
+    this.currentDate = new Date();
+    this.selectedDate.set(this.currentDate);
 
     this.attandences = [
       {
@@ -46,5 +56,56 @@ export class AttendancesComponent implements OnInit {
         id: 1,
       },
     ];
+
+    this.primengConfig.setTranslation({
+      firstDayOfWeek: 1,
+      dayNames: [
+        'Sonntag',
+        'Montag',
+        'Dienstag',
+        'Mittwoch',
+        'Donnerstag',
+        'Freitag',
+        'Samstag',
+      ],
+      dayNamesShort: ['Son', 'Mon', 'Die', 'Mit', 'Don', 'Fre', 'Sam'],
+      dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+      monthNames: [
+        'Januar',
+        'Februar',
+        'März',
+        'April',
+        'Mai',
+        'Juni',
+        'Juli',
+        'August',
+        'September',
+        'Oktober',
+        'November',
+        'Dezember',
+      ],
+      monthNamesShort: [
+        'Jan',
+        'Feb',
+        'Mär',
+        'Apr',
+        'Mai',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Okt',
+        'Nov',
+        'Dez',
+      ],
+      today: 'Heute',
+      clear: 'Leeren',
+      dateFormat: 'dd.mm.yy',
+      weekHeader: 'Wo',
+    });
+  }
+
+  saveAttendance(): void {
+    console.log(this.selectedDate);
   }
 }
