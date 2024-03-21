2b8a2f3b27e0d925e5d6863fbff73ab463da00ee
HAD <heinrich.adam@sanalogic.com>
2024-03-21 11:09:03 +0100

    Worked on user.component

--- ColtUI/src/app/users/users.component.ts
+++ ColtUI/src/app/users/users.component.ts
@@ -8,12 +8,13 @@ import { GlobalStore } from '../core/stores/global.store';
 import { LayoutComponent } from '../core/layout/layout.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DataViewModule } from 'primeng/dataview';
+import { TagModule } from 'primeng/tag';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, DataViewModule],
+  imports: [CommonModule, ReactiveFormsModule, DataViewModule, TagModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
